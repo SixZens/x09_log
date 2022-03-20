8773f77d0ef522bddcc96a5742f2f081aefdafa9
SixZens <90531233+SixZens@users.noreply.github.com>
2022-03-20 16:14:55 +0700

    Create DataCode.lua

---
+++ DataCode.lua
@@ -0,0 +1,22 @@
+local Code = {
+    "Sub2Fer999",
+    "Enyo_is_Pro",
+    "Magicbus",
+    "JCWK",
+    "Starcodeheo",
+    "Bluxxy",
+    "fudd10_v2",
+    "3BVISITS",
+    "UPD16",
+    "FUDD10",
+    "BIGNEWS",
+    "Sub2OfficialNoobie",
+    "SUB2GAMERROBOT_EXP1",
+    "StrawHatMaine",
+    "SUB2NOOBMASTER123",
+    "Sub2Daigrock",
+    "Axiore",
+    "TantaiGaming",
+    "STRAWHATMAINE",
+}
+return Code
